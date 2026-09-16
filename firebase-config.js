674806bb57afd15fa91c045c8f23b55b9dd12e09
Daniil022 <158138582+Daniil022@users.noreply.github.com>
2026-09-17 00:28:59 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// ⚠️ Firebase конфиг
+// Можно публиковать — apiKey публичный по дизайну Google
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAjP2Ny_xRIhMwoVZ-HWiW7nxlAIUV3HIo",
+  authDomain: "sk-terminal.firebaseapp.com",
+  projectId: "sk-terminal",
+  storageBucket: "sk-terminal.firebasestorage.app",
+  messagingSenderId: "286319815156",
+  appId: "1:286319815156:web:1d5901ca091894477271a1"
+};
